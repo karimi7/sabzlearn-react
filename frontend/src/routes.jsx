63b2eec1b533cc48decd4e77271ecba6a39c8c9e
Mohaddese Karimi <mhaddese@gmail.com>
2026-05-29 16:14:02 +0330

    update1

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,8 +1,8 @@
-import Index from './pages/Index/Index.jsx';
-import CourseInfo from './pages/CourseInfo/CourseInfo.jsx';
-import Category from './pages/Category/Category.jsx';
-import ArticleInfo from './pages/ArticleInfo/ArticleInfo.jsx';
-import Courses from './pages/Courses/Courses.jsx';
+import Index from './Pages/Index/Index.jsx';
+import CourseInfo from './Pages/CourseInfo/CourseInfo.jsx';
+import Category from './Pages/Category/Category.jsx';
+import ArticleInfo from './Pages/ArticleInfo/ArticleInfo.jsx';
+import Courses from './Pages/Courses/Courses.jsx';
 import Login from './Pages/Login/Login.jsx';
 import Register from './Pages/Register/Register.jsx';
 
